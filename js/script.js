4e01bb467d11ec4255b1d9f41b35d538dd1195a2
Daniya Yelemessova <daniya@collingsmail.com>
2023-11-03 21:01:36 -0400

    Add branching

--- js/script.js
+++ js/script.js
@@ -3,13 +3,29 @@ window.addEventListener("load", function(){
   form.addEventListener("submit", infoGathering)
 })
 
+function classHidden(){
+  document.querySelector("#detail-front").setAttribute("class", "hidden");
+  document.querySelector("#detail-back").setAttribute("class", "hidden");
+  document.querySelector("#detail-full").setAttribute("class", "hidden");
+
+}
+
 function infoGathering(e) {
   e.preventDefault();
+  classHidden()
   const inputName = document.getElementById("name").value;
   const output = document.getElementById("output");
   const radioInput = document.querySelector("input[name=personality]:checked").value;
   const dropInput = document.getElementById("web-path").value;
-  output.innerText = `Hi, ${inputName}! Happy to see you here! I see that you have ${radioInput} personality and your preferred role in web development is ${dropInput}.`
+  output.innerText = `Hi, ${inputName}! Happy to see you here! I see that you have ${radioInput} personality and your preferred role in web development is ${dropInput}.`;
+
+  if(radioInput === "detail-oriented" && dropInput === "Front-end"){
+    document.querySelector("#detail-front").removeAttribute("class");
+  }else if(radioInput === "detail-oriented" && dropInput === "Back-end"){
+    document.querySelector("#detail-back").removeAttribute("class");
+  }else if(radioInput === "detail-oriented" && dropInput === "Full-stack"){
+    document.querySelector("#detail-full").removeAttribute("class");
+  }
   
 
 }
